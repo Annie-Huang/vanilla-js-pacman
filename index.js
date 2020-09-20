da3a14cbe10bb442f5160c87cd05d7332e84dff9
Annie Huang <annie.hn@gmail.com>
2020-09-20 10:22:23 +1000

    Handle basic setup when use clicks on startButton

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {LEVEL, OBJECT_TYPE} from './setup';
 // Classes
 import GameBoard from './GameBoard';
+import Pacman from './Pacman';
 
 // DOM Element
 const gameGrid = document.querySelector('#game');
@@ -27,9 +28,17 @@ function checkCollision(pacman, ghosts) {}
 
 function gameLoop(pacman, ghosts) {}
 
-function startGame() {}
+function startGame() {
+  gameWin = false;
+  powerPillActive = false;
+  score = 0;
 
+  startButton.classList.add('hide');
 
+}
+
+// Initialize game
+startButton.addEventListener('click', startGame);
 
 
 
